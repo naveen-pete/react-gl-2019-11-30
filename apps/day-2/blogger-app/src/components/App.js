1f8d7d2d17e1ee8ff968beac79e608222e21dbfb
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-12-01 10:04:31 +0530

    commit blogger app at the start of day 2

---
+++ apps/day-2/blogger-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './Header';
+import Posts from './Posts';
+
+const App = () => {
+  return <div>
+    <Header />
+    <Posts />
+  </div>;
+};
+
+export default App;
